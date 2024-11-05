024d7fe42774fd88a66dd6208280bbee54ed85c0
Kirisukyoto <108452971+kirisutokyoto0@users.noreply.github.com>
2024-11-05 15:35:26 +0000

    Daghan changes ang Graph.js

--- graph-coloring-game/src/App.js
+++ graph-coloring-game/src/App.js
@@ -1,13 +1,20 @@
 // src/App.js
-import React from 'react';
-import Graph from './Graph';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Graph from "./Graph";
 
 const App = () => {
-  return (
-    <div className="App">
-      <Graph />
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/graph" element={<Graph />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
